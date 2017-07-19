9712c433533a6b5cecfebf184a5cab83d4fef061
wenxiaoning <wenxiaoning@gochinatv.com>
2017-07-19 16:55:46 +0800

    增加wx支持接口1.2

--- app/views/api/wx.py
+++ app/views/api/wx.py
@@ -8,7 +8,6 @@ from app.config import app
 from app.common.base import BaseResponse
 from flask import Blueprint
 from flask import request
-import hashlib
 
 wx_bp = Blueprint('wx', __name__)
 
@@ -16,29 +15,13 @@ wx_bp = Blueprint('wx', __name__)
 @wx_bp.route('/wx/mp_callback')
 def mp_callback():
     '''公众号回调地址'''
-
-    # ([('signature', '9d22b7bfcc46bceb77e362eba295ab79355cf139'),
-    #   ('timestamp', '1500452603'), ('echostr', '12831846290135416827'),
-    #   ('nonce', '3889653278')])
-
-    if request.method == 'GET':
-        return request.args.get('echostr')
+    app.logger.debug(request.remote_addr)
+    app.logger.debug(request.method)
     app.logger.debug(request.args)
     app.logger.debug(request.json)
     app.logger.debug(request.form)
+    app.logger.debug(request.data)
+    if request.method == 'GET':
+        return request.args.get('echostr', "success")
 
-    timestamp = '1500452603'
-    echostr = '12831846290135416827'
-    signature = '9d22b7bfcc46bceb77e362eba295ab79355cf139'
-    nonce = '3889653278'
-    token = 'F3uIXPvYZ8nmpPVhazFkert'
-    list = [token, timestamp, nonce,'F3uIXPvYZ8nmpPVhazFkertj25fry8OcINM87xRe4kg']
-    list.sort()
-
-    sha1 = hashlib.sha1()
-    # map(sha1.update, list)
-    sha1.update("".join(list))
-    hashcode = sha1.hexdigest()
-    app.logger.debug(hashcode)
-
-    return ''
+    return BaseResponse.return_success()
